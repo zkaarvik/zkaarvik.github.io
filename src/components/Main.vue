006419f708e2cf5e34cfbc9f5eb37229bc1cc8c3
Zach Kaarvik <zkaarvik@gmail.com>
2021-03-20 19:31:45 -0700

    Make basic

---
+++ src/components/Main.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <h1>Zach Kaarvik</h1>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Main',
+
+    data: () => ({
+    }),
+  }
+</script>
